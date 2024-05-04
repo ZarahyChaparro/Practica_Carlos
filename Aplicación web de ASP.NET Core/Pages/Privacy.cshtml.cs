3ef9c04c2fac9417342f2b717a033e0acb658451
Zarahy Chaparro <chaparrozarahy@gmail.com>
2024-05-03 19:15:28 -0500

    Proyecto Carlos

---
+++ Aplicación web de ASP.NET Core/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aplicación_web_de_ASP.NET_Core.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
